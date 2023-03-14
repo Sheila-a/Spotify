b7acf32cc363dba221f83173808dfd3fa8479faa
Sheila-a <ugwuodo20@gmail.com>
2023-03-14 20:58:55 +0100

    fix redirect url

--- server_side/server.js
+++ server_side/server.js
@@ -33,7 +33,7 @@ app.post("/refresh", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  const code = req.body.access_token;
+  const code = req.body.code;
   const spotifyApi = new SpotifyCoreApi({
     redirectUri: process.env.REDIRECT_URI,
     clientId: process.env.CLIENT_ID,
